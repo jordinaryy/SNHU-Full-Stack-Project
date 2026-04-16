60dc8734064092d04e88659f4f00f3d51212a6d3
jordinaryy <jordankrueger25@gmail.com>
2026-04-16 04:16:34 -0400

    module 6 completed baseline

--- app.js
+++ app.js
@@ -30,7 +30,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/travel', travelRouter);
-//app.use('/users', usersRouter);
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
 app.use('/api', apiRouter);
 
 
--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -34,13 +34,62 @@ const tripsFindByCode = async (req, res) => {
     }
 };
 
-module.exports = {
-    tripsList,
-    tripsFindByCode
+const tripsAddTrip = async (req, res) => {
+    const q = await Model
+        .create({
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        });
+    console.log(q);
+    if (!q) {
+        return res
+        .status(400)
+        .json(err);
+    } else {
+        return res
+        .status(201)
+        .json(q);
+    }
 };
 
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.params);
+    console.log(req.body);
+    const q = await Model
+        .findOneAndUpdate(
+            { 'code': req.params.tripCode },
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }
+        )
+        .exec();
+    if (!q) {
+        return res
+        .status(400)
+        .json(err);
+    } else {
+        return res
+        .status(201)
+        .json(q);
+    }
+};
 
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
